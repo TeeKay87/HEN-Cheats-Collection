0c35e67f48d84b812df32d061c91e0a5a8d9fe14
TeeKay87 <pcgmr87@gmail.com>
2026-06-01 03:47:03 +0200

    Update app.js

--- list/app.js
+++ list/app.js
@@ -46,7 +46,7 @@ const HASH_SEPARATOR = '-';
 const COVERART_SIZE = '384';
 const COVERART_SUFFIX = '?w=' + COVERART_SIZE + '&thumb=false';
 const COVERART_SUFFIX_HERO = '?w=1024&thumb=false';
-const COVERART_FALLBACK = 'https://images.seeklogo.com/logo-png/42/1/playstation-logo-png_seeklogo-428627.png';
+const COVERART_FALLBACK = 'https://img.icons8.com/3d-fluency/1200/playstation.jpg';
 const MINIMUM_CHARS_FOR_SEARCH = 2;
 
 const RENDER_BATCH_SIZE = 60;
